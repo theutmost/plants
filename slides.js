ba1073702778ce8b31fd22615f071fe13f689d8b
“Sanni <sanniyouboty@gmail.com>
2018-11-17 13:29:39 -0800

    fixing gitignore issue

--- slides.js
+++ slides.js
@@ -14,13 +14,40 @@ let zInd = 1;
 // WANT: when click in slide area, change slide number based on z-index
 slideArea.addEventListener("click", function() {
   currentSlide = currentSlide + 1;
-  zInd = zInd + 1;
 
   //making the images loop
   if (currentSlide > images.length - 1) {
     currentSlide = 0;
   }
 
-  //pick the right image
+  zInd = zInd + 1;
+
+  // removing animation from the style for EVERY IMG
+  images.forEach(function(image) {
+    image.style.animation = "";
+  });
+
+  //pick the right image (+ adding fade)
   images[currentSlide].style.zIndex = zInd;
+  images[currentSlide].style.animation = "fade 0.5s";
+});
+
+//------------------------------------
+
+// when i mouse over the imgs area, puts the images in a random place
+
+slideArea.addEventListener("mouseover", function() {
+  console.log("you are mousing over");
+
+  // loop over ea img and put it in a random place
+  images.forEach(image => {
+    // Picking a random # for ea individual img
+    const x = 100 * Math.random() - 50;
+    const y = 100 * Math.random() - 50;
+
+    // Change how the img looks, using TRANSLATE
+    // image.style.transform = "translate (50px, 60px)";
+    image.style.transform = `translate (${x}px, ${y}px)`;
+    console.log("you are in forEach end");
+  });
 });
